2b36ad56cd7da55c205e387bb90072d7d7412046
Ifeoluwa <ifel1939@gmail.com>
2023-04-11 06:11:27 -0700

    concat

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((arr) => {
+for (const arr of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(arr);
-})
+}
