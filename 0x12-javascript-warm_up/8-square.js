8fd0524392c40094d2a4464f466841ff7f3062ab
Ifeoluwa <ifel1939@gmail.com>
2023-04-11 11:05:42 -0700

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing size');
-  const x = parseInt(process.argv[2])
+	console.log('Missing size');
+	const x = parseInt(process.argv[2])
 } else {
-     const x = parseInt(process.argv[2])
-  for (let i = 0; i < x; i++) {
-    let row = '';
-    for (let j = 0; j < x; j++) row += 'X';
-    console.log(row);
-  }
+	const x = parseInt(process.argv[2])
+	for (let i = 0; i < x; i++) {
+		let square = '';
+		for (let j = 0; j < x; j++) square += 'X';
+		console.log(square);
+	}
 }
