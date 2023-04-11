1f04423da6203474a0cd49136c25bbe961b671d7
Ifeoluwa <ifel1939@gmail.com>
2023-04-11 11:15:07 -0700

    print add

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function factorial (x) {
-  if (x < 0) {
+function factorial (n) {
+  if (n < 0) {
     return (-1);
   }
-  if (x === 0 || isNaN(n)) {
+  if (n === 0 || isNaN(n)) {
     return (1);
   }
-  return (x * factorial(x - 1));
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(parseInt(process.argv[2])));
