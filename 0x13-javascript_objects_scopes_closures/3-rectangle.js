6ac46395aff3a684b15c6cace4480913e265dbb3
Ifeoluwa <ifel1939@gmail.com>
2023-04-12 05:20:13 -0700

    join files

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,14 +8,13 @@ class Rectangle {
   }
 
   print () {
-	  let s = '';
     for (let i = 0; i < this.height; i++) {
+      let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
-	s += '';
+      console.log(s);
     }
-	 console.log(s);
   }
 }
 
